028e29924ad5a4218cd524aa822b21e0c8062fdd
amr0mengd <amrmengd0@protonmail.com>
2026-02-14 23:31:24 +0200

    added the  navbar div and the logo

---
+++ src/components/NavBar.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="navbar" class="container mx-auto py-4.25 flex items-center justify-between">
+    <div id="logo" class="flex items-center justify-center space-x-2">
+      <router-link to="#">
+        <img src="../assets/logo.png" class="w-14" />
+      </router-link>
+      <p class="text-[32px] font-bold text-primary">Egypt-View</p>
+    </div>
+  </div>
+</template>
