44ea12c30aaf1323cf4ce180c9136fb268e7dee7
amr0mengd <amrmengd0@protonmail.com>
2026-02-19 05:34:20 +0200

    in progress with the navbar , made 2 components for the navigation item and the language switcher, also added a div separator in between

---
+++ src/components/LanguageSwitcher.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="w-10 h-6 rounded-sm text-[8px]">
+    <router-link
+      to="#"
+      :class="{ 'bg-primary text-white underline': isActive, 'bg-white text-primary': !isActive }"
+      >EN</router-link
+    >
+    <router-link
+      to="#"
+      :class="{ 'bg-primary text-white underline': isActive, 'bg-white text-primary': !isActive }"
+      >JP</router-link
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['isActive'],
+}
+</script>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,7 +4,30 @@
       <router-link to="#">
         <img src="../assets/logo.png" class="w-14" />
       </router-link>
-      <p class="text-[32px] font-bold text-primary">Egypt-View</p>
+      <p class="text-[32px] font-bold text-primary pb-0.5">Egypt-View</p>
+    </div>
+    <div class="space-x-6 flex">
+      <div id="navigation" class="flex space-x-8">
+        <nav-item text="Home" :is-active="true"></nav-item>
+        <div>
+          <nav-item text="Services"></nav-item>
+        </div>
+        <nav-item text="Gallery"></nav-item>
+        <nav-item text="About"></nav-item>
+        <nav-item text="Contact"></nav-item>
+      </div>
+      <div id="separator" class="w-px h-6 bg-[#D9D9D9] rounded-[5px]"></div>
+      <div id="switcher">
+        <LanguageSwitcher/>
+      </div>
     </div>
   </div>
 </template>
+
+<script>
+import NavItem from './NavItem.vue'
+import LanguageSwitcher from './LanguageSwitcher.vue';
+export default {
+  components: { NavItem, LanguageSwitcher },
+}
+</script>
