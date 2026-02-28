093c7d19f7320956a34ca0f283df91b7c23b0cac
amr0mengd <amrmengd0@protonmail.com>
2026-02-28 17:34:05 +0200

    added all navigation routes and paths to the router

--- src/router/router.js
+++ src/router/router.js
@@ -6,6 +6,13 @@ import HomePage from "@/pages/HomePage.vue";
 // lazy router component can be used to only load the component when the user visits it not initially
 // that can improve the app as it doesn't have to load all components initially
 // const HomePage = ()=> import("../pages/HomePage.vue")
+const GalleryPage = () => import("../pages/GalleryPage.vue");
+const AboutPage = () => import("../pages/AboutPage.vue");
+const ContactPage = () => import("../pages/ContactPage.vue");
+const ToursPage = () => import("../pages/services pages/ToursPage.vue");
+const TranslationPage = () => import("../pages/services pages/TranslationPage.vue");
+const BrandPage = () => import("../pages/services pages/BrandPage.vue");
+const EventsPage = () => import("../pages/services pages/EventsPage.vue");
 
 const router = createRouter({
 	history: createWebHashHistory(),
@@ -14,6 +21,41 @@ const router = createRouter({
 			name: "home",
 			path: "/",
 			component: HomePage,
+		},
+		{
+			name: "gallery",
+			path: "/gallery",
+			component: GalleryPage,
+		},
+		{
+			name: "about",
+			path: "/about",
+			component: AboutPage,
+		},
+		{
+			name: "contact",
+			path: "/contact",
+			component: ContactPage,
+		},
+		{
+			name: "tours",
+			path: "/tours",
+			component: ToursPage,
+		},
+		{
+			name: "translation",
+			path: "/translation",
+			component: TranslationPage,
+		},
+		{
+			name: "brand",
+			path: "/brand",
+			component: BrandPage,
+		},
+		{
+			name: "events",
+			path: "/events",
+			component: EventsPage,
 		},
 	],
 });
