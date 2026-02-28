dc9bf09117a5c9a237629c0735cafd8df7571b7e
amr0mengd <amrmengd0@protonmail.com>
2026-02-28 16:06:13 +0200

    added i18n folder and used it in the main.js

---
+++ src/i18n/i18n.js
@@ -0,0 +1,15 @@
+import { createI18n } from "vue-i18n";
+import en from "./en.json";
+import jp from "./jp.json";
+
+const i18n = createI18n({
+	legacy: false,
+	locale: "en",
+	fallbackLocale: "en",
+	messages: {
+		en,
+		jp,
+	},
+});
+
+export default i18n;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import router from "./router/router";
+import i18n from "./i18n/i18n";
 // import { createPinia } from 'pinia'
 
 import "./main.css";
@@ -9,5 +10,6 @@ const app = createApp(App);
 
 // app.use(createPinia())
 app.use(router);
+app.use(i18n);
 
 app.mount("#app");
