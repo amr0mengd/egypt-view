f41ff0d97022175140cc02ec84b665263c6841e0
amr0mengd <amrmengd0@protonmail.com>
2026-02-28 16:11:16 +0200

    added the dynamic switching language function with dynamic classes

--- src/components/ui/LanguageSwitcher.vue
+++ src/components/ui/LanguageSwitcher.vue
@@ -2,19 +2,23 @@
 	<div class="border-primary flex items-center rounded-sm border text-[8px]">
 		<button
 			@click="setLang('en')"
-			class="bg-primary px-1 py-1.5 font-bold text-white underline">
+			class="cursor-pointer px-1 py-1.5"
+			:class="{ 'bg-primary font-bold text-white underline': locale === 'en' }">
 			EN
 		</button>
-		<button @click="setLang('jp')" class="px-1 py-1.5">JP</button>
+		<button
+			@click="setLang('jp')"
+			class="cursor-pointer px-1 py-1.5"
+			:class="{ 'bg-primary font-bold text-white underline': locale === 'jp' }">
+			JP
+		</button>
 	</div>
 </template>
 
 <script setup>
-import { ref } from "vue";
-
-const activeLang = ref("en");
-
-function setLang(lang = "en") {
-	activeLang.value = lang;
+import { useI18n } from "vue-i18n";
+const { locale } = useI18n({ useScope: "global" });
+function setLang(lang) {
+	locale.value = lang;
 }
 </script>
