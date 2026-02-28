8ccee0469a4c5c57764018a862ac7703557e26b3
amr0mengd <amrmengd0@protonmail.com>
2026-02-28 19:07:56 +0200

    added pinia in the main js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 import { createApp } from "vue";
 import router from "./router/router";
 import i18n from "./i18n/i18n";
-// import { createPinia } from 'pinia'
+import { createPinia } from "pinia";
 
 import "./main.css";
 import App from "./App.vue";
 
 const app = createApp(App);
 
-// app.use(createPinia())
+app.use(createPinia());
 app.use(router);
 app.use(i18n);
 
