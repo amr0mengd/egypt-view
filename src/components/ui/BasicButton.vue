7ab55af5bdb193f15dd73d0a8ce059b4d30e9106
amr0mengd <amrmengd0@protonmail.com>
2026-03-02 07:36:20 +0200

    added a slot basicbuttton component with the inverted prop for all buttons in the site

---
+++ src/components/ui/BasicButton.vue
@@ -0,0 +1,22 @@
+<template>
+	<router-link
+		class="bg-primary cursor-pointer rounded-xl px-6 py-4 text-xl"
+		:class="{ 'text-primary bg-primary/10': inverted, 'text-white': !inverted }"
+		:to="to">
+		<slot></slot>
+	</router-link>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+defineProps({
+	inverted: {
+		type: Boolean,
+		default: false,
+	},
+	to: {
+		type: String,
+		required: true,
+	},
+});
+</script>
