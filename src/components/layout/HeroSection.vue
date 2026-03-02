8b21daec3af3eb3cca279cef4be4d6841ef9610e
amr0mengd <amrmengd0@protonmail.com>
2026-03-02 07:37:35 +0200

    created a hero section layout component and added it in the homepage component

---
+++ src/components/layout/HeroSection.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<div id="heroText & CTA's" class="flex">
+			<div id="hero article">
+				<h1>{{ t("heroTitle") }}</h1>
+				<p>{{ t("heroSubtitle") }}</p>
+			</div>
+			<div id="CTA's">
+				<BasicButton to="/about">About Us</BasicButton>
+				<BasicButton to="/contact" :inverted="true">Book Now</BasicButton>
+			</div>
+		</div>
+		<div id="heroImage" class="rounded-xl">
+			<router-link to="#"><img src="../../assets/hero.jpg" alt="camel" /></router-link>
+		</div>
+	</div>
+</template>
+
+<script setup>
+import BasicButton from "../ui/BasicButton.vue";
+import { useI18n } from "vue-i18n";
+const { t } = useI18n();
+</script>
