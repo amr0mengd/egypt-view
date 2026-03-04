872305526ad365e31b6f3b132d6f5ec536164ba8
amr0mengd <amrmengd0@protonmail.com>
2026-03-04 08:50:32 +0200

    the hover and active effect in the basicbutton component

--- src/components/ui/BasicButton.vue
+++ src/components/ui/BasicButton.vue
@@ -1,6 +1,6 @@
 <template>
 	<router-link
-		class="bg-primary cursor-pointer rounded-xl px-6 py-4 text-xl"
+		class="bg-primary cursor-pointer rounded-2xl px-6 py-4 text-xl font-bold hover:shadow-[0_2px_12px_rgba(0,0,0,0.25)] active:shadow-[0_1px_6px_rgba(0,0,0,0.3)]"
 		:class="{ 'text-primary bg-primary/10': inverted, 'text-white': !inverted }"
 		:to="to">
 		<slot></slot>
@@ -8,7 +8,6 @@
 </template>
 
 <script setup>
-import { defineProps } from "vue";
 defineProps({
 	inverted: {
 		type: Boolean,
