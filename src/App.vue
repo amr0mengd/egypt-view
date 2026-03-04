9f9fd70166c496ed90d75d334de2720b3955be02
amr0mengd <amrmengd0@protonmail.com>
2026-03-04 08:49:06 +0200

    added a parent div to the navbar component to apply shadow with full page width

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-	<NavBar class="w-full"></NavBar>
+	<div class="shadow-[0_2px_10px_rgba(0,0,0,0.05)]">
+		<NavBar class="w-full"></NavBar>
+	</div>
 	<router-view></router-view>
 </template>
 
