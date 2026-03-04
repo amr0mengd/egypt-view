4e4a720a7f8b5df81781dbddcaea867300a54922
amr0mengd <amrmengd0@protonmail.com>
2026-03-04 10:19:14 +0200

    added a content section component with the heading and the styling

---
+++ src/components/layout/ContentSection.vue
@@ -0,0 +1,37 @@
+<template>
+	<h1 class="py-2.5 text-center text-5xl font-medium">{{ t("servicesHeading") }}</h1>
+	<div class="container mx-auto py-12 space-y-18">
+		<ContentItem
+			:title="t('toursTitle')"
+			:subTitle="t('toursSubtitle')"
+			to="/tours"
+			:src="toursImg" />
+		<ContentItem
+			:title="t('interpretationsTitle')"
+			:subTitle="t('interpretationsSubtitle')"
+			to="/translation"
+			:src="interpretationsImg"
+			:inverted="true" />
+		<ContentItem
+			:title="t('eventsTitle')"
+			:subTitle="t('eventsSubtitle')"
+			to="/events"
+			:src="eventsImg" />
+		<ContentItem
+			:title="t('souvenirsTitle')"
+			:subTitle="t('souvenirsSubtitle')"
+			to="/brand"
+			:src="souvenirsImg"
+			:inverted="true" />
+	</div>
+</template>
+
+<script setup>
+import { useI18n } from "vue-i18n";
+import ContentItem from "../ui/ContentItem.vue";
+import toursImg from "../../assets/tours.png";
+import interpretationsImg from "../../assets/interpretations.png";
+import eventsImg from "../../assets/events.png";
+import souvenirsImg from "../../assets/souvenirs.jpg";
+const { t } = useI18n();
+</script>
