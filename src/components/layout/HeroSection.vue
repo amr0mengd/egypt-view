845eb9c9b0f680f8ea2b7b286a653798b7df1f59
amr0mengd <amrmengd0@protonmail.com>
2026-03-04 08:50:06 +0200

    done with the hero section sizing and typo and also cta styling with hover and active effect

--- src/components/layout/HeroSection.vue
+++ src/components/layout/HeroSection.vue
@@ -1,17 +1,19 @@
 <template>
-	<div>
-		<div id="heroText & CTA's" class="flex">
-			<div id="hero article">
-				<h1>{{ t("heroTitle") }}</h1>
-				<p>{{ t("heroSubtitle") }}</p>
+	<div id="heroSection" class="container mx-auto flex h-165 space-x-8 pt-16 pb-18">
+		<div id="heroText & CTA's" class="w-4/12 space-y-28">
+			<div id="hero article" class="space-y-9 pt-12">
+				<h1 class="text-[64px]/[100%] font-bold">{{ t("heroTitle") }}</h1>
+				<p class="font-normal text-[22px]/[33px]">{{ t("heroSubtitle") }}</p>
 			</div>
-			<div id="CTA's">
+			<div id="CTA's" class="space-x-6">
 				<BasicButton to="/about">About Us</BasicButton>
 				<BasicButton to="/contact" :inverted="true">Book Now</BasicButton>
 			</div>
-		</div>
-		<div id="heroImage" class="rounded-xl">
-			<router-link to="#"><img src="../../assets/hero.jpg" alt="camel" /></router-link>
+		</div> 
+		<div id="heroImage" class="h-full w-8/12">
+			<router-link to="#" class="block h-full"
+				><img class="h-full w-full rounded-xl object-cover" src="../../assets/hero.jpg" alt="camel"
+			/></router-link>
 		</div>
 	</div>
 </template>
