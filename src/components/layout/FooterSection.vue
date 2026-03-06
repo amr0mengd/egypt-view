89a7c94fc8d2501a6c2f3591d9c609fc543e082a
amr0mengd <amrmengd0@protonmail.com>
2026-03-06 07:37:12 +0200

    created a footer section component to be used in the app.vue file and done with the footer content with all links and responsiveness

---
+++ src/components/layout/FooterSection.vue
@@ -0,0 +1,43 @@
+<template>
+	<div>
+		<hr class="text-primary bg-primary h-0.5" />
+	</div>
+	<div id="footer" class="mx-72 flex justify-around h-60">
+		<div id="footer logo" class="flex h-full  flex-col items-center justify-center space-y-8">
+			<router-link to="#"><img src="../../assets/logo.png" width="120px" alt="" /></router-link>
+			<p class="text-primary text-center text-[22px]">{{ t("footerLogo") }}</p>
+		</div>
+		<div id="links" class="flex py-8 justify-center space-x-28 text-xl">
+			<div id="social links" class="space-y-7">
+				<h3 class="text-[22px] font-bold text-[#6B6B6B]">{{ t("social") }}</h3>
+				<ul class="space-y-3">
+					<li class="hover:text-primary hover:underline active:text-white">
+						<a target="_blank" href="https://www.facebook.com/Egypt.View.tourism/">{{ t("facebook") }}</a>
+					</li>
+					<li class="hover:text-primary hover:underline active:text-white">
+						<a target="_blank" href="https://x.com/egypt_view">{{ t("twitter") }}</a>
+					</li>
+					<li class="hover:text-primary hover:underline active:text-white">
+						<a target="_blank" href="https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=info@egypt-view.com">{{ t("mail") }}</a>
+					</li>
+				</ul>
+			</div>
+			<div id="company links" class="space-y-7">
+				<h3 class="text-[22px] font-bold text-[#6B6B6B]">{{ t("company") }}</h3>
+				<ul class="space-y-3">
+					<li class="hover:text-primary hover:underline active:text-white">
+						<router-link to="/about">{{ t("CTA1") }}</router-link>
+					</li>
+					<li class="hover:text-primary hover:underline active:text-white">
+						<router-link to="/contact">{{ t("contact") }}</router-link>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup>
+import { useI18n } from "vue-i18n";
+const { t } = useI18n();
+</script>
