d88f6ee7bb62ad628bda9beb868c5ad49984933c
amr0mengd <amrmengd0@protonmail.com>
2026-03-06 07:38:44 +0200

    added the footer component to the main app component also added navbar styling to stick it to the top

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
-	<div class="shadow-[0_2px_10px_rgba(0,0,0,0.05)]">
-		<NavBar class="w-full"></NavBar>
+	<div class="shadow-[0_2px_10px_rgba(0,0,0,0.05)] sticky top-0">
+		<NavBar class="w-full bg-white"></NavBar>
 	</div>
 	<router-view></router-view>
+  <FooterSection/>
 </template>
 
 <script setup>
+import FooterSection from "./components/layout/FooterSection.vue";
 import NavBar from "./components/layout/NavBar.vue";
 </script>
