4fa6ddea50528297bfba220648a203960f9e129b
amr0mengd <amrmengd0@protonmail.com>
2026-03-06 07:39:26 +0200

    used cta i18n locale text

--- src/components/layout/HeroSection.vue
+++ src/components/layout/HeroSection.vue
@@ -3,13 +3,13 @@
 		<div id="heroText & CTA's" class="w-4/12 space-y-28">
 			<div id="hero article" class="space-y-9 pt-12">
 				<h1 class="text-[64px]/[100%] font-bold">{{ t("heroTitle") }}</h1>
-				<p class="font-normal text-[22px]/[33px]">{{ t("heroSubtitle") }}</p>
+				<p class="text-[22px]/[33px] font-normal">{{ t("heroSubtitle") }}</p>
 			</div>
 			<div id="CTA's" class="space-x-6">
-				<BasicButton to="/about">About Us</BasicButton>
-				<BasicButton to="/contact" :inverted="true">Book Now</BasicButton>
+				<BasicButton to="/about">{{ t("CTA1") }}</BasicButton>
+				<BasicButton to="/contact" :inverted="true">{{ t("CTA2") }}</BasicButton>
 			</div>
-		</div> 
+		</div>
 		<div id="heroImage" class="h-full w-8/12">
 			<router-link to="#" class="block h-full"
 				><img class="h-full w-full rounded-xl object-cover" src="../../assets/hero.jpg" alt="camel"
