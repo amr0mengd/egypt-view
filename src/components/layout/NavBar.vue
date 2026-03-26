4eb36eebe0767255f5187d88e1fcaa961b95c58c
amr0mengd <amrmengd0@protonmail.com>
2026-03-26 23:07:30 +0200

    added a wrapper to the navbar to make it full width

--- src/components/layout/NavBar.vue
+++ src/components/layout/NavBar.vue
@@ -1,17 +1,19 @@
 <template>
-	<div id="navbar" class="text-primary container mx-auto flex items-center justify-between py-4">
-		<router-link to="/" id="logo" class="flex items-center justify-center space-x-2">
-			<img src="../../assets/logo.png" class="w-14" />
-			<p class="text-[32px] font-bold">{{ t("heading") }}</p>
-		</router-link>
-		<div class="flex items-center space-x-4">
-			<NavItem to="/" :text="t('home')"></NavItem>
-			<ServicesDropdown />
-			<NavItem to="/gallery" :text="t('gallery')"></NavItem>
-			<NavItem to="/about" :text="t('about')"></NavItem>
-			<NavItem to="/contact" :text="t('contact')"></NavItem>
-			<div id="divider" class="h-6 w-px rounded-[5px] bg-[#D9D9D9]"></div>
-			<LanguageSwitcher />
+	<div class="w-full">
+		<div id="navbar" class="text-primary container mx-auto flex items-center justify-between py-4">
+			<router-link to="/" id="logo" class="flex items-center justify-center space-x-2">
+				<img src="../../assets/logo.png" class="w-14" />
+				<p class="text-[32px] font-bold">{{ t("heading") }}</p>
+			</router-link>
+			<div class="flex items-center space-x-4">
+				<NavItem to="/" :text="t('home')"></NavItem>
+				<ServicesDropdown />
+				<NavItem to="/gallery" :text="t('gallery')"></NavItem>
+				<NavItem to="/about" :text="t('about')"></NavItem>
+				<NavItem to="/contact" :text="t('contact')"></NavItem>
+				<div id="divider" class="h-6 w-px rounded-[5px] bg-[#D9D9D9]"></div>
+				<LanguageSwitcher />
+			</div>
 		</div>
 	</div>
 </template>
