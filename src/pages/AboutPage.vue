f62886c1e3185276411f02f15804f3f5d28c72c4
amr0mengd <amrmengd0@protonmail.com>
2026-03-26 23:17:28 +0200

    in progress with the about page

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,3 +1,15 @@
 <template>
-  <h1>about</h1>
+	<div id="about page" class="space-y-8">
+		<div id="hero image" class="h-128 w-full shadow-[0_4px_10px_rgba(0,0,0,0.25)]">
+			<img
+				src="../assets/aboutHero.png"
+				class="h-full w-full object-cover object-bottom"
+				alt="Temple" />
+		</div>
+		<div id="about content" class="h-screen bg-gray-300">
+			<div id="hero title">
+				<p class="text-primary text-center text-sm font-medium">WHO WE ARE</p>
+			</div>
+		</div>
+	</div>
 </template>
