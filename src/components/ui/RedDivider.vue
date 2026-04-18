564f14b4c3833becd352690af647b0ac2ba158fb
amr0mengd <amrmengd0@protonmail.com>
2026-04-18 11:49:40 +0200

    added RedDivider component to minimize code

---
+++ src/components/ui/RedDivider.vue
@@ -0,0 +1,24 @@
+<template>
+	<div id="divider" class="flex" :class="{ 'justify-center': justify === 'center' }">
+		<hr
+			class="h-0.75 w-20 rounded-2xl"
+			:class="{
+				'bg-white text-white/90': color === 'white',
+				'text-primary bg-primary': color === 'red',
+			}" />
+	</div>
+</template>
+
+<script setup>
+defineProps({
+	justify: {
+		type: String,
+		required: true,
+	},
+	color: {
+		type: String,
+		required: false,
+		default: "red",
+	},
+});
+</script>
