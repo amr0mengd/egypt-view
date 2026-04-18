c9efa4af5da635739c6fdbce8b92d97b2b031ccd
amr0mengd <amrmengd0@protonmail.com>
2026-04-18 11:47:32 +0200

    added CTA component for every CTA section in every page

---
+++ src/components/layout/CTA.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="bg-primary relative flex flex-col items-center justify-center rounded-2xl py-10 overflow-hidden">
+		<div class="space-y-4 text-center text-white">
+			<h2 class="text-[40px] font-medium">{{ t("explore") }}</h2>
+			<p class="text-xl font-medium">{{ t("adventure") }}</p>
+		</div>
+		<BasicButton :inverted="true" to="contact" class="bg-white/90 mt-10">Book Now</BasicButton>
+		<IconSVG :name="svg" class=" absolute -bottom-12 -right-6"/>
+	</div>
+</template>
+
+<script setup>
+import { useI18n } from "vue-i18n";
+import BasicButton from "../ui/BasicButton.vue";
+import IconSVG from "../ui/IconSVG.vue";
+const { t } = useI18n();
+defineProps({
+	svg: String,
+});
+</script>
