788322dc60943f90ff3d5d7ddc4609501b80f8c9
amr0mengd <amrmengd0@protonmail.com>
2026-04-18 11:50:17 +0200

    redPill component with dynamic SVG icon

---
+++ src/components/ui/RedPill.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="bg-primary/10 text-primary rounded-full mt-1 border-2 px-6 py-2.5">
+		<div class="flex items-center space-x-2">
+			<IconSVG :name="svg" class="h-7 w-7" />
+			<p class="text-xl">{{ pillText }}</p>
+		</div>
+	</div>
+</template>
+
+<script setup>
+import IconSVG from "./IconSVG.vue";
+
+defineProps({
+	pillText: {
+		type: String,
+		required: true,
+	},
+	svg: String,
+});
+</script>
