fc16e3f3ca864c381c67fa1bf2526176f03961b6
amr0mengd <amrmengd0@protonmail.com>
2026-04-22 18:07:16 +0200

    added ui component for the red square with title in the team page

---
+++ src/components/ui/RedBlockTitle.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="flex items-center space-x-2.5">
+		<span class="bg-primary inline-block h-3 w-3"></span>
+		<h1 class="text-2xl leading-9 font-semibold">{{ title }}</h1>
+	</div>
+</template>
+
+<script setup>
+defineProps({
+	title: {
+		type: String,
+		required: true,
+	},
+});
+</script>
