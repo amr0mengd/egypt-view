471ec630c27801ca93104efe6c82464fc377cf8b
amr0mengd <amrmengd0@protonmail.com>
2026-05-05 14:00:57 +0300

    added contactForm component to make the form in the contact page dynamic

---
+++ src/components/ui/ContactForm.vue
@@ -0,0 +1,42 @@
+<template>
+	<label
+		:for="id"
+		class="text-sm font-semibold tracking-[0.7px] text-[#555555]"
+		>{{ label }}</label
+	>
+	<input
+		v-if="type !== 'textarea'"
+		:type="type"
+		:id="id"
+		:name="id"
+		:placeholder="placeholder"
+		class="border-primary mt-1.5 w-full rounded-lg border bg-white px-4 py-2" />
+	<textarea
+		v-if="type === 'textarea'"
+		:type="type"
+		:id="id"
+		:name="id"
+		:placeholder="placeholder"
+		class="border-primary mt-1.5 w-full rounded-lg border bg-white px-4 py-2" />
+</template>
+
+<script setup>
+defineProps({
+	id: {
+		type: String,
+		required: true,
+	},
+	type: {
+		type: String,
+		required: true,
+	},
+	placeholder: {
+		type: String,
+		required: true,
+	},
+	label: {
+		type: String,
+		required: true,
+	},
+});
+</script>
