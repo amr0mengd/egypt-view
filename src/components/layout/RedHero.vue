bd5597e2badd8975bb61b438ef0b5bc807ae192b
amr0mengd <amrmengd0@protonmail.com>
2026-05-05 14:00:15 +0300

    ran npm format

--- src/components/layout/RedHero.vue
+++ src/components/layout/RedHero.vue
@@ -1,6 +1,8 @@
 <template>
-	<div class="to-primary flex min-h-90 justify-around bg-linear-to-r from-[#8B0000]">
-		<div class="flex flex-col items-center justify-center space-y-2 font-bold text-white">
+	<div
+		class="to-primary flex min-h-90 justify-around bg-linear-to-r from-[#8B0000]">
+		<div
+			class="flex flex-col items-center justify-center space-y-2 font-bold text-white">
 			<p class="text-sm tracking-[2px]">{{ smallHead }}</p>
 			<h1 class="text-[64px]">{{ Headline }}</h1>
 			<p v-if="phrase" class="text-xl font-semibold">{{ phrase }}</p>
