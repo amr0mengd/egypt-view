67925cfa63122ad60b760583015861242f22b9f4
amr0mengd <amrmengd0@protonmail.com>
2026-05-05 14:01:09 +0300

    format

--- src/components/ui/RedCard.vue
+++ src/components/ui/RedCard.vue
@@ -1,5 +1,6 @@
 <template>
-	<div class="bg-primary/10 border-primary space-y-10 rounded-2xl border-2 px-16 py-6 mt-8 mb-10">
-    <slot></slot>
-  </div>
+	<div
+		class="bg-primary/10 border-primary mt-8 mb-10 space-y-10 rounded-2xl border-2 px-16 py-6">
+		<slot></slot>
+	</div>
 </template>
