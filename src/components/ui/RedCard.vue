6ce5dc533faab8a7153c1884da55216afeaaf2e8
amr0mengd <amrmengd0@protonmail.com>
2026-05-06 11:10:13 +0300

    added container mx-auto to the red card component

--- src/components/ui/RedCard.vue
+++ src/components/ui/RedCard.vue
@@ -1,6 +1,6 @@
 <template>
 	<div
-		class="bg-primary/10 border-primary mt-8 mb-10 space-y-10 rounded-2xl border-2 px-16 py-6">
+		class="bg-primary/10 border-primary container mx-auto mt-8 mb-10 space-y-10 rounded-2xl border-2 px-16 py-6">
 		<slot></slot>
 	</div>
 </template>
