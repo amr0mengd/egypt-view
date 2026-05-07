08ed2ba515df4a48d24e518a8d50a309bd9887d0
amr0mengd <amrmengd0@protonmail.com>
2026-05-07 12:49:38 +0300

    done with translation page

--- src/pages/services pages/TranslationPage.vue
+++ src/pages/services pages/TranslationPage.vue
@@ -1,1 +1,24 @@
-<template>Translation</template>
+<template>
+	<RedHero :smallHead="t('ourServices')" :Headline="t('translation&interpretation')" />
+	<RedCard>
+		<div class="space-y-6">
+			<RedBlockTitle :title="t('translationTitle')" />
+			<div class="space-y-1">
+				<p class="font-medium text-lg"><RightArrow /> {{ t("translation1") }}</p>
+				<p class="font-medium text-lg"><RightArrow /> {{ t("translation2") }}</p>
+				<p class="font-medium text-lg"><RightArrow /> {{ t("translation3") }}</p>
+				<p class="font-medium text-lg"><RightArrow /> {{ t("translation4") }}</p>
+			</div>
+			<div>
+				<h3 class="text-xl font-medium text-[#555555] leading-7.5">{{ t("translationParagraph") }}</h3>
+			</div>
+		</div>
+	</RedCard>
+	<CTA svg="pharaoh" />
+</template>
+
+<script setup>
+import CTA from "@/components/layout/CTA.vue";
+import { useI18n } from "vue-i18n";
+const { t } = useI18n();
+</script>
