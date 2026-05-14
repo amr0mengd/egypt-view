4d25f94c279332c49af92b8a1210042e1e14eb56
amr0mengd <amrmengd0@protonmail.com>
2026-05-15 00:50:21 +0300

    .

--- src/router/router.js
+++ src/router/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory} from "vue-router";
+import { createRouter, createWebHistory} from "vue-router";
 
 // this import method loads every imported component initially
 import HomePage from "@/pages/HomePage.vue";
@@ -17,7 +17,7 @@ const EventsPage = () => import("../pages/services pages/EventsPage.vue");
 const TeamMember = () => import("../pages/TeamMember.vue");
 
 const router = createRouter({
-	history: createWebHashHistory(),
+	history: createWebHistory(import.meta.env.BASE_URL),
 	routes: [
 		{
 			name: "home",
