7b1a97e5c34b2812f373f94d06f59d9655b90465
amr0mengd <amrmengd0@protonmail.com>
2026-05-15 00:52:51 +0300

    discard last 2 changes

--- src/router/router.js
+++ src/router/router.js
@@ -17,7 +17,7 @@ const EventsPage = () => import("../pages/services pages/EventsPage.vue");
 const TeamMember = () => import("../pages/TeamMember.vue");
 
 const router = createRouter({
-	history: createWebHistory(import.meta.env.BASE_URL),
+	history: createWebHistory(),
 	routes: [
 		{
 			name: "home",
