c1583f992a9ca37a4ed1370d41b8a75f051019b8
amr0mengd <amrmengd0@protonmail.com>
2026-05-20 13:30:53 +0300

    done with the condition overlay image in team page

--- src/components/layout/RedHero.vue
+++ src/components/layout/RedHero.vue
@@ -1,12 +1,13 @@
 <template>
 	<div
-		class="to-primary flex min-h-90 justify-around bg-linear-to-r from-[#8B0000]">
-		<div
-			class="flex flex-col items-center justify-center space-y-2 font-bold text-white">
-			<p class="text-sm tracking-[2px]">{{ smallHead }}</p>
-			<h1 class="text-[64px]">{{ Headline }}</h1>
-			<p v-if="phrase" class="text-xl font-semibold">{{ phrase }}</p>
-		</div>
+		class="to-primary relative flex min-h-90 flex-col items-center justify-center space-y-2 bg-linear-to-r from-[#8B0000] font-bold text-white">
+		<img
+			v-if="img"
+			src="../../assets/teamHero.jpg"
+			class="absolute inset-0 z-5 h-full w-full object-cover object-[0_25%] opacity-25" />
+		<p class="z-10 text-sm tracking-[2px]">{{ smallHead }}</p>
+		<h1 class="z-10 text-[64px]">{{ Headline }}</h1>
+		<p v-if="phrase" class="text-xl font-semibold">{{ phrase }}</p>
 	</div>
 </template>
 
@@ -21,5 +22,6 @@ defineProps({
 		required: true,
 	},
 	phrase: String,
+	img: Boolean,
 });
 </script>
